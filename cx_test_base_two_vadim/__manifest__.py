3e50484698396664c3f5815fb6781ab1e9de5eab
Vadim <ahmetzianovvadim@yandex.ru>
2022-06-21 18:22:51 +0300

    [ADD] module init

---
+++ cx_test_base_two_vadim/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "cx_test_base_two_vadim",
+    "version": "14.0.1.0.0",
+    "category": "Uncategorized",
+    "website": "https://cetmix.com",
+    "author": "ahmetzianovvadim",
+    "license": "LGPL-3",
+    "depends": ["base"],
+    "data": [],
+}
